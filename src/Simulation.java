41c17027506900f78f1cb7941ec86c4e188c0674
Clayton Marr <marr.54@buckeyemail.osu.edu>
2024-01-15 02:07:13 -0500

    fixed error in making derivation graph for run with no stages

--- src/Simulation.java
+++ src/Simulation.java
@@ -344,14 +344,14 @@ public class Simulation {
 	// returns 2d graph -- in [rows = rule time step + 1 for each stage] [ columns = etyma] 
 	// to use for printing a .csv file where each column is an etymon
 	// and each row is its form after the operation of rule number [row number - 1]
-			// this relationsihp will change by 1 whenever a stage is passed once intermediate stage insertion/deletion is covered -- but it is not covered yet. 
+			// this relationship will change by 1 whenever a stage is passed once intermediate stage insertion/deletion is covered -- but it is not covered yet. 
 	// when intermediate stage insertion/deletion is covered, it *will be* "ABSENT" (or UTILS.ABSENT_REPR) for times at which something is absent i.e. fell out of use, not loaned yet ... etc.
 	// but this is not covered yet. 
 	// cells have... 
 	// "{new form}" whenever an etymon is changed by a rule
 	// "+ {new form}" whenever an etymon is inserted in an intermediate stage -- once this is covered... 
 	// if form is unchanged by the latest rule, so as not to explode file size, the cell will be empty.
-	//TODO note that at present, this may not handle insertion/deletion of etyma at stages exactly properly. 
+	//TODO note that at present, this may not handle insertion/deletion of etyma at stages exactly properly. The goal is to fix this. 
 	public String[][] derivationGraph()
 	{
 		String[][] output = new String[1 + ruleEffects.length + NUM_GOLD_STAGES + NUM_BLACK_STAGES][1 + NUM_ETYMA];
@@ -364,60 +364,67 @@ public class Simulation {
 		int bsi = 0, gsi = 0; // black stage instance, gold stage instance. ... for when we cover intermediate stage insertion/deletion. 
 		int rule_i = 0; // current rule timestep 		
 		int outrow_i = 1; // current output column index; which is number of rules plus number of stages past, plus 1 
-		boolean nextStageBlack = stagesOrdered[soi].charAt(0) == 'b'; 
-		int next_stage_ri = (nextStageBlack ? blackStageInstants : goldStageInstants)
-				[Integer.parseInt(stagesOrdered[soi].substring(1))]; 
-
-		while (soi >= stagesOrdered.length ? false : !stagesOrdered[soi].equals("out"))
+		
+		// current policy: do the following with the idea being that it is as to deal with the possible situation of inserted or deleted etyma
+			// can ( and should ) skip if there is no stages at all.  
+		if (stagesOrdered.length > 0) 
 		{
-			if (rule_i == next_stage_ri) 
+			
+			
+			boolean nextStageBlack = stagesOrdered[soi].charAt(0) == 'b'; 
+			int next_stage_ri = (nextStageBlack ? blackStageInstants : goldStageInstants)
+					[Integer.parseInt(stagesOrdered[soi].substring(1))]; 
+	
+			while (soi >= stagesOrdered.length ? false : !stagesOrdered[soi].equals("out"))
 			{
-				boolean stageIsBlack = stagesOrdered[soi].charAt(0) == 'b'; 
-				
-				//TODO debugging
-				System.out.println("stage loc detected; " +
-						(stageIsBlack ? UTILS.BLACK_STAGENAME_FLAG + blackStageNames[bsi]
-								: UTILS.GOLD_STAGENAME_FLAG + goldStageNames[gsi]) 
-						+"... (n stages : "+(NUM_GOLD_STAGES + NUM_BLACK_STAGES)); 
-			
-				output[outrow_i][0] = stageIsBlack ? UTILS.BLACK_STAGENAME_FLAG + blackStageNames[bsi]
-						: UTILS.GOLD_STAGENAME_FLAG + goldStageNames[gsi]; 
-				Lexicon this_stage_lexicon = stageIsBlack ? blackStageResultLexica[bsi] : goldStageResultLexica[gsi]; 
-				for (int eti = 0 ; eti < NUM_ETYMA; eti++)	
+				if (rule_i == next_stage_ri) 
 				{
-					String stagewise_reflex = this_stage_lexicon.getByID(eti).print(); 
-					if (output[outrow_i-1][1 + eti].equals(UTILS.ABSENT_REPR) 
-							&& !stagewise_reflex.equals(UTILS.ABSENT_REPR)) 
-						output[outrow_i][1 + eti] = "+ " ;
-					output[outrow_i][1 + eti] += stagewise_reflex; 
-				}
-						
-				soi ++; outrow_i++; 
-				if (stageIsBlack)	bsi++; 
-				else	gsi++; 
+					boolean stageIsBlack = stagesOrdered[soi].charAt(0) == 'b'; 
+					
+					//TODO debugging
+					System.out.println("stage loc detected; " +
+							(stageIsBlack ? UTILS.BLACK_STAGENAME_FLAG + blackStageNames[bsi]
+									: UTILS.GOLD_STAGENAME_FLAG + goldStageNames[gsi]) 
+							+"... (n stages : "+(NUM_GOLD_STAGES + NUM_BLACK_STAGES)); 
 				
-				if (soi >= stagesOrdered.length ? false : !stagesOrdered[soi].equals("out"))
-				{
-					nextStageBlack = stagesOrdered[soi].charAt(0) == 'b'; 
-					next_stage_ri = (nextStageBlack ? blackStageInstants : goldStageInstants)
-							[Integer.parseInt(stagesOrdered[soi].substring(1))]; 	
+					output[outrow_i][0] = stageIsBlack ? UTILS.BLACK_STAGENAME_FLAG + blackStageNames[bsi]
+							: UTILS.GOLD_STAGENAME_FLAG + goldStageNames[gsi]; 
+					Lexicon this_stage_lexicon = stageIsBlack ? blackStageResultLexica[bsi] : goldStageResultLexica[gsi]; 
+					for (int eti = 0 ; eti < NUM_ETYMA; eti++)	
+					{
+						String stagewise_reflex = this_stage_lexicon.getByID(eti).print(); 
+						if (output[outrow_i-1][1 + eti].equals(UTILS.ABSENT_REPR) 
+								&& !stagewise_reflex.equals(UTILS.ABSENT_REPR)) 
+							output[outrow_i][1 + eti] = "+ " ;
+						output[outrow_i][1 + eti] += stagewise_reflex; 
+					}
+							
+					soi ++; outrow_i++; 
+					if (stageIsBlack)	bsi++; 
+					else	gsi++; 
+					
+					if (soi >= stagesOrdered.length ? false : !stagesOrdered[soi].equals("out"))
+					{
+						nextStageBlack = stagesOrdered[soi].charAt(0) == 'b'; 
+						next_stage_ri = (nextStageBlack ? blackStageInstants : goldStageInstants)
+								[Integer.parseInt(stagesOrdered[soi].substring(1))]; 	
+					}
+					else	break; 
 				}
-				else	break; 
-			}
-			
-			output[outrow_i][0] = "R"+rule_i; 
-			
-			for (int eti=0; eti < NUM_ETYMA; eti++)
-			{
-				String rule_eff_here = ruleEffects[rule_i][eti]; 
-				int arrow_loc = rule_eff_here.indexOf("> ");
-				if (arrow_loc == -1) // no change -- then take previous form. 
-					output[outrow_i][1 + eti] = ""; // "^"; // output[outrow_i-1][1 + eti].replace("> ",""); 
-				else	// there was a change: cell will have "> [new form]" 
-					output[outrow_i][1 + eti] = rule_eff_here.substring(arrow_loc+2, rule_eff_here.indexOf(";")); 
-			}
-			outrow_i++; rule_i++ ; 
-			
+				
+				output[outrow_i][0] = "R"+rule_i; 
+				
+				for (int eti=0; eti < NUM_ETYMA; eti++)
+				{
+					String rule_eff_here = ruleEffects[rule_i][eti]; 
+					int arrow_loc = rule_eff_here.indexOf("> ");
+					if (arrow_loc == -1) // no change -- then take previous form. 
+						output[outrow_i][1 + eti] = ""; // "^"; // output[outrow_i-1][1 + eti].replace("> ",""); 
+					else	// there was a change: cell will have "> [new form]" 
+						output[outrow_i][1 + eti] = rule_eff_here.substring(arrow_loc+2, rule_eff_here.indexOf(";")); 
+				}
+				outrow_i++; rule_i++ ; 
+			}				
 		}
 		
 		// cover remaining rules, no risk of insertion or deletion of etyma anymore, or stages being hit... 
