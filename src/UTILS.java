b7dbf6879ffd46f6edd201cb5a3a25444478462c
Clayton Marr <marr.54@buckeyemail.osu.edu>
2024-07-22 13:53:54 -0400

    remove spaces from alpha checking

--- src/UTILS.java
+++ src/UTILS.java
@@ -1093,6 +1093,9 @@ public class UTILS {
 			String curpp = ""+protophones[ppi].trim();
 			if(curpp.charAt(0) == '[')
 			{
+				// as of July 2024, spaces in feature matrices as written are ignored: 
+				curpp = curpp.replace(" ", "");
+				
 				String[] specs = curpp.substring(1, curpp.indexOf(']')).split(""+FEAT_DELIM); 
 				for (String spec : specs) 
 					if (spec_is_alpha_marked(spec))	return true; 
